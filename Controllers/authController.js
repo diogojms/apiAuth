badb3ed1635fb814036303ef0b02d67e9415f529
diogojms <a29986@alunos.ipca.pt>
2023-11-20 23:21:51 +0000

    updates

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -15,67 +15,108 @@ const authController = {
   register : async (req, res) => {
     const { email, name, nif, username, password, dubPassword } = req.body;
   
-    if (!email) {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Email necessário', User: null });
-      return res.status(422).json({ msg: "Email necessário" });
-    }
+    // if (!email) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Email necessário', User: null });
+    //   return res.status(422).json({ msg: "Email necessário" });
+    // }
+
+    // if (!name) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Nome necessário', User: null });
+    //   return res.status(422).json({ msg: "Nome necessário" });
+    // }
     
-    if (!username) {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Username necessário', User: null });
-      return res.status(422).json({ msg: "Username necessário" });
-    }
+    // if (!username) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Username necessário', User: null });
+    //   return res.status(422).json({ msg: "Username necessário" });
+    // }
     
-    if (!password) {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Password necessária', User: null });
-      return res.status(422).json({ msg: "Password necessária" });
-    }
+    // if (!password) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Password necessária', User: null });
+    //   return res.status(422).json({ msg: "Password necessária" });
+    // }
     
-    if (dubPassword != password) {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Passwords não condizem', User: null });
-      return res.status(422).json({ msg: "Passwords não condizem" });
-    }
+    // if (dubPassword != password) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Passwords não condizem', User: null });
+    //   return res.status(422).json({ msg: "Passwords não condizem" });
+    // }
     
-    if (!name) {
-      //generate random name
-    }
-    const userExists = await Auth.findOne({ username });
+    // const userExists = await Auth.findOne({ username });
     
-    if (userExists) {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Username já existe', User: null });
-      return res.status(422).json({ msg: "Username já existe" });
-    }
+    // if (userExists) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'Username já existe', User: null });
+    //   return res.status(422).json({ msg: "Username já existe" });
+    // }
+
+    // const nifExists = await Auth.findOne({ nif });
+
+    // if(nifExists) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'NIF já existe', User: null });
+    //   return res.status(422).json({ msg: "NIF já existe" });
+    // }
+
+    // const emailExists = await Auth.findOne({ email });
+    
+    // if(emailExists) {
+    //   SendToLog({ Level: 'Error', Action: '/auth/register', Description: 'email já existe', User: null });
+    //   return res.status(422).json({ msg: "Email já existe" });
+    // }
+
+    const validateAndLogError = (field, errorMsg) => {
+      SendToLog({ Level: 'Error', Action: '/auth/register', Description: errorMsg, User: null });
+      return res.status(422).json({ msg: errorMsg });
+    };
+    
+    if (!email) return validateAndLogError('Email', 'Email necessário');
+    if (!name) return validateAndLogError('Name', 'Nome necessário');
+    if (!username) return validateAndLogError('Username', 'Username necessário');
+    if (!password) return validateAndLogError('Password', 'Password necessária');
+    if (dubPassword !== password) return validateAndLogError('Password', 'Passwords não condizem');
+    
     const salt = await bcrypt.genSalt(12);
     const hash = await bcrypt.hash(password, salt);
     
-    const auth = new Auth({
-        username,
-        password: hash
-      });
-      let generatedId;
-      try {
-        generatedId = await auth.save();
-      } catch (err) {
-        SendToLog({ Level: 'Error', Action: '/auth/register', Description: err, User: null });
-        return res.status(500).json({ msg: err });
-    }
+    const checkIfExists = async (field, value, errorMsg) => {
+      const exists = await Auth.findOne({ [field]: value });
+      if (exists){
+        return validateAndLogError(field, errorMsg);
+      } else{
+        const auth = new Auth({
+          username,
+          password: hash
+        });
+        let generatedId;
+        try {
+          generatedId = await auth.save();
+        } catch (err) {
+          SendToLog({ Level: 'Error', Action: '/auth/register', Description: err, User: null });
+          return res.status(500).json({ msg: err });
+      }
+  
+      const user = new User({
+          _id: generatedId,
+          email,
+          name,
+          nif,
+          role: 1
+        });
+        try {
+          await user.save();
+          SendToLog({ Level: 'Info', Action: '/auth/register', Description: req.body, User: null });
+          return res.status(200).json(req.body);
+        } catch (err) {
+          SendToLog({ Level: 'Error', Action: '/auth/register', Description: err, User: null });
+          return res.status(500).json({ msg: err });
+      }
+      }
+    };
+    
+    await checkIfExists('username', username, 'Username já existe');
+    await checkIfExists('nif', nif, 'NIF já existe');
+    await checkIfExists('email', email, 'Email já existe');
 
-    const user = new User({
-        _id: generatedId,
-        email,
-        name,
-        nif,
-        role: 1
-      });
-      try {
-        await user.save();
-        SendToLog({ Level: 'Info', Action: '/auth/register', Description: req.body, User: null });
-        return res.status(200).json(req.body);
-      } catch (err) {
-        SendToLog({ Level: 'Error', Action: '/auth/register', Description: err, User: null });
-        return res.status(500).json({ msg: err });
-    }
+    
+    
   },
-
   login: async (req, res) => {
     const { username, password } = req.body;
     if (!username) {
