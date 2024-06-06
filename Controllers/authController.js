72bc395f4f867818c9234c73daad5c2201d7b9ae
goncalo.maciel <goncalo.maciel@proactive.swiss>
2024-06-06 21:09:01 +0100

    img

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,8 +1,8 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Auth = require("../Models/auth");
 const User = require("../Models/user");
-const { SendToLog } = require('../logs');
+const { SendToLog } = require("../logs");
 const { checkToken } = require("../utils");
 const { getIdFromToken } = require("../utils");
 
@@ -14,70 +14,90 @@ const revokedTokens = [];
 
 const authController = {
   /**
- * @swagger
- * /register:
- *   post:
- *     summary: Register a new user
- *     description: Endpoint to register a new user with the provided information.
- *     tags:
- *       - Authentication
- *     requestBody:
- *       description: User registration data
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *               name:
- *                 type: string
- *               nif:
- *                 type: string
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *               dubPassword:
- *                 type: string
- *               role:
- *                 type: string
- *             required:
- *               - email
- *               - name
- *               - username
- *               - password
- *               - role
- *     responses:
- *       '200':
- *         description: User successfully registered
- *       '422':
- *         description: Unprocessable Entity - Invalid input data
- *       '500':
- *         description: Internal Server Error - Registration failed
- */
+   * @swagger
+   * /register:
+   *   post:
+   *     summary: Register a new user
+   *     description: Endpoint to register a new user with the provided information.
+   *     tags:
+   *       - Authentication
+   *     requestBody:
+   *       description: User registration data
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               email:
+   *                 type: string
+   *               name:
+   *                 type: string
+   *               nif:
+   *                 type: string
+   *               username:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *               dubPassword:
+   *                 type: string
+   *               role:
+   *                 type: string
+   *             required:
+   *               - email
+   *               - name
+   *               - username
+   *               - password
+   *               - role
+   *     responses:
+   *       '200':
+   *         description: User successfully registered
+   *       '422':
+   *         description: Unprocessable Entity - Invalid input data
+   *       '500':
+   *         description: Internal Server Error - Registration failed
+   */
   register: async (req, res) => {
-    const { email, name, nif, address, city, zipcode, phone, username, password, dubPassword, role } = req.body;
+    const {
+      email,
+      name,
+      nif,
+      address,
+      city,
+      zipcode,
+      phone,
+      username,
+      password,
+      dubPassword,
+      role,
+    } = req.body;
 
     const validateAndLogError = (field, errorMsg) => {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: errorMsg, User: null });
+      SendToLog({
+        Level: "Error",
+        Action: "/auth/register",
+        Description: errorMsg,
+        User: null,
+      });
       if (!res.headersSent) {
         return res.status(422).json({ msg: errorMsg });
       }
     };
 
-    if (!email) return validateAndLogError('Email', 'Email necessário');
-    if (!name) return validateAndLogError('Name', 'Nome necessário');
-    if (!nif) return validateAndLogError('Nif', 'Nif necessário');
-    if (!address) return validateAndLogError('Address', 'Address necessário');
-    if (!city) return validateAndLogError('City', 'City necessário');
-    if (!zipcode) return validateAndLogError('Zipcode', 'Zipcode necessário');
-    if (!phone) return validateAndLogError('Phone', 'Phone necessário');
-    if (!username) return validateAndLogError('Username', 'Username necessário');
-    if (!password) return validateAndLogError('Password', 'Password necessária');
-    if (!role) return validateAndLogError('Role', 'Role necessária');
-    if (dubPassword !== password) return validateAndLogError('Password', 'Passwords não condizem');
+    if (!email) return validateAndLogError("Email", "Email necessário");
+    if (!name) return validateAndLogError("Name", "Nome necessário");
+    if (!nif) return validateAndLogError("Nif", "Nif necessário");
+    if (!address) return validateAndLogError("Address", "Address necessário");
+    if (!city) return validateAndLogError("City", "City necessário");
+    if (!zipcode) return validateAndLogError("Zipcode", "Zipcode necessário");
+    if (!phone) return validateAndLogError("Phone", "Phone necessário");
+    if (!username)
+      return validateAndLogError("Username", "Username necessário");
+    if (!password)
+      return validateAndLogError("Password", "Password necessária");
+    if (!role) return validateAndLogError("Role", "Role necessária");
+    if (dubPassword !== password)
+      return validateAndLogError("Password", "Passwords não condizem");
 
     try {
       const existsUsername = await Auth.findOne({ username });
@@ -85,10 +105,12 @@ const authController = {
       const existsEmail = await Auth.findOne({ email });
       const existsPhone = await Auth.findOne({ phone });
 
-      if (existsUsername) return validateAndLogError('Username', 'Username já existe');
-      if (existsNif) return validateAndLogError('NIF', 'NIF já existe');
-      if (existsEmail) return validateAndLogError('Email', 'Email já existe', 401);
-      if (existsPhone) return validateAndLogError('phone', 'Phone já existe');
+      if (existsUsername)
+        return validateAndLogError("Username", "Username já existe");
+      if (existsNif) return validateAndLogError("NIF", "NIF já existe");
+      if (existsEmail)
+        return validateAndLogError("Email", "Email já existe", 401);
+      if (existsPhone) return validateAndLogError("phone", "Phone já existe");
 
       const salt = await bcrypt.genSalt(12);
       const hash = await bcrypt.hash(password, salt);
@@ -114,12 +136,22 @@ const authController = {
 
       await user.save();
 
-      SendToLog({ Level: 'Info', Action: '/auth/register', Description: req.body, User: null });
+      SendToLog({
+        Level: "Info",
+        Action: "/auth/register",
+        Description: req.body,
+        User: null,
+      });
       if (!res.headersSent) {
         return res.status(200).json(req.body);
       }
     } catch (err) {
-      SendToLog({ Level: 'Error', Action: '/auth/register', Description: err, User: null });
+      SendToLog({
+        Level: "Error",
+        Action: "/auth/register",
+        Description: err,
+        User: null,
+      });
       if (!res.headersSent) {
         return res.status(500).json({ msg: err });
       }
@@ -127,58 +159,58 @@ const authController = {
   },
 
   /**
- * @swagger
- * /login:
- *   post:
- *     summary: Authenticate user and generate JWT token
- *     description: Endpoint to authenticate a user using their username and password, and generate a JWT token.
- *     tags:
- *       - Authentication
- *     requestBody:
- *       description: User login data
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *             required:
- *               - username
- *               - password
- *     responses:
- *       '200':
- *         description: Authentication successful, JWT token generated
- *       '401':
- *         description: Unauthorized - Revoked token used
- *       '404':
- *         description: Not Found - Username not found
- *       '422':
- *         description: Unprocessable Entity - Invalid username or password
- *       '500':
- *         description: Internal Server Error - Authentication failed
- */
+   * @swagger
+   * /login:
+   *   post:
+   *     summary: Authenticate user and generate JWT token
+   *     description: Endpoint to authenticate a user using their username and password, and generate a JWT token.
+   *     tags:
+   *       - Authentication
+   *     requestBody:
+   *       description: User login data
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               username:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *             required:
+   *               - username
+   *               - password
+   *     responses:
+   *       '200':
+   *         description: Authentication successful, JWT token generated
+   *       '401':
+   *         description: Unauthorized - Revoked token used
+   *       '404':
+   *         description: Not Found - Username not found
+   *       '422':
+   *         description: Unprocessable Entity - Invalid username or password
+   *       '500':
+   *         description: Internal Server Error - Authentication failed
+   */
   login: async (req, res) => {
     const { username, password } = req.body;
-    console.log(req.body)
+    console.log(req.body);
     if (!username) {
       const logData = {
-        Level: 'Error',
+        Level: "Error",
         Action: `/auth/login`,
-        Description: 'Username is required',
-        User: null
+        Description: "Username is required",
+        User: null,
       };
       SendToLog(logData);
       return res.status(422).json({ msg: "Username is required" });
     } else if (!password) {
       const logData = {
-        Level: 'Error',
+        Level: "Error",
         Action: `/auth/login`,
-        Description: 'Password is required',
-        User: null
+        Description: "Password is required",
+        User: null,
       };
       SendToLog(logData);
       return res.status(422).json({ msg: "Password is required" });
@@ -186,10 +218,10 @@ const authController = {
     const auth = await Auth.findOne({ username });
     if (!auth) {
       const logData = {
-        Level: 'Error',
+        Level: "Error",
         Action: `/auth/login`,
-        Description: 'Username not found',
-        User: null
+        Description: "Username not found",
+        User: null,
       };
       SendToLog(logData);
       return res.status(404).json({ msg: "Username not found" });
@@ -197,10 +229,10 @@ const authController = {
     const isPasswordValid = await bcrypt.compare(password, auth.password);
     if (!isPasswordValid) {
       const logData = {
-        Level: 'Error',
+        Level: "Error",
         Action: `/auth/login`,
-        Description: 'Invalid password',
-        User: null
+        Description: "Invalid password",
+        User: null,
       };
       SendToLog(logData);
       return res.status(422).json({ msg: "Invalid password" });
@@ -211,39 +243,49 @@ const authController = {
       const token = jwt.sign(
         {
           id: auth._id,
-          role: user.role
+          role: user.role,
+          nif: user.nif,
+          name: user.name,
+          email: user.email,
+          phone: user.phone,
+          img: user.img,
         },
         secret,
         {
-          expiresIn: tokenValidation
+          expiresIn: tokenValidation,
         }
       );
       if (revokedTokens.includes(token)) {
         const logData = {
-          Level: 'Error',
+          Level: "Error",
           Action: `/auth/logout`,
-          Description: 'Revoked token used',
-          User: null
+          Description: "Revoked token used",
+          User: null,
         };
         SendToLog(logData);
         return res.status(401).json({ msg: "Revoked token used" });
       } else {
         const logData = {
-          Level: 'Info',
+          Level: "Info",
           Action: `/auth/login`,
           Description: token,
-          User: null
+          User: null,
         };
         SendToLog(logData);
-        res.status(200).json({ token, tokenValidation, name: user.name, id: user.id, city: user.city });
+        res.status(200).json({
+          token,
+          tokenValidation,
+          name: user.name,
+          id: user.id,
+          city: user.city,
+        });
       }
-
     } catch (err) {
       const logData = {
-        Level: 'Error',
+        Level: "Error",
         Action: `/auth/login`,
         Description: err,
-        User: null
+        User: null,
       };
       SendToLog(logData);
       res.status(500).json({ msg: err });
@@ -290,18 +332,18 @@ const authController = {
     checkToken,
     async (req, res) => {
       const { password, newPassword } = req.body;
-      const authHeader = req.headers['authorization'];
+      const authHeader = req.headers["authorization"];
       const token = authHeader && authHeader.split(" ")[1];
 
       // Check if password is provided
       if (!password) {
-        const error = { msg: 'Password necessária' };
+        const error = { msg: "Password necessária" };
         return res.status(422).json(error);
       }
 
       // Check if new password is provided
       if (!newPassword) {
-        const error = { msg: 'Nova password necessária' };
+        const error = { msg: "Nova password necessária" };
         return res.status(422).json(error);
       }
 
@@ -310,7 +352,7 @@ const authController = {
 
       // Check if authentication exists
       if (!auth) {
-        const error = { msg: 'Autenticação necessária' };
+        const error = { msg: "Autenticação necessária" };
         return res.status(404).json(error);
       }
 
@@ -318,7 +360,7 @@ const authController = {
 
       // Check if password is valid
       if (!isPasswordValid) {
-        const error = { msg: 'Password inválida' };
+        const error = { msg: "Password inválida" };
         return res.status(422).json(error);
       }
 
@@ -335,77 +377,77 @@ const authController = {
         if (revokedTokens.includes(token)) {
           // Token revogado, trate a solicitação de acordo
           const logData = {
-            Level: 'Error',
+            Level: "Error",
             Action: `/auth/logout`,
-            Description: 'Revoked token used',
-            User: null
+            Description: "Revoked token used",
+            User: null,
           };
           SendToLog(logData);
           return res.status(401).json({ msg: "Revoked token used" });
         } else {
           const logData = {
-            Level: 'Info',
-            Action: '/auth/password',
+            Level: "Info",
+            Action: "/auth/password",
             Description: updatedUser,
-            User: userId
+            User: userId,
           };
 
           SendToLog(logData);
 
           return res.status(200).json({ updatedUser });
         }
-
       } catch (err) {
         const logData = {
-          Level: 'Error',
-          Action: '/auth/password',
+          Level: "Error",
+          Action: "/auth/password",
           Description: err,
-          User: userId
+          User: userId,
         };
 
         SendToLog(logData);
 
         return res.status(500).json({ msg: err });
       }
-    }
+    },
   ],
   // Rota para logout
   /**
- * @swagger
- * /logout:
- *   post:
- *     summary: Logout user
- *     description: Endpoint to log out the authenticated user, revoking the current session token.
- *     tags:
- *       - Authentication
- *     security:
- *       - BearerAuth: []
- *     responses:
- *       '200':
- *         description: Logout successful, session token revoked
- *       '401':
- *         description: Unauthorized - Invalid or revoked token used
- *     headers:
- *       - name: Authorization
- *         in: header
- *         description: Bearer token for authentication
- *         required: true
- *         schema:
- *           type: string
- *           format: "Bearer {token}"
- */
+   * @swagger
+   * /logout:
+   *   post:
+   *     summary: Logout user
+   *     description: Endpoint to log out the authenticated user, revoking the current session token.
+   *     tags:
+   *       - Authentication
+   *     security:
+   *       - BearerAuth: []
+   *     responses:
+   *       '200':
+   *         description: Logout successful, session token revoked
+   *       '401':
+   *         description: Unauthorized - Invalid or revoked token used
+   *     headers:
+   *       - name: Authorization
+   *         in: header
+   *         description: Bearer token for authentication
+   *         required: true
+   *         schema:
+   *           type: string
+   *           format: "Bearer {token}"
+   */
   logout: [
-    checkToken, async (req, res) => {
-      const authHeader = req.headers['authorization'];
+    checkToken,
+    async (req, res) => {
+      const authHeader = req.headers["authorization"];
       const token = authHeader && authHeader.split(" ")[1];
 
       revokedTokens.push(token);
 
       const logData = {
-        Level: 'Info',
+        Level: "Info",
         Action: `/auth/logout`,
-        Description: 'User logged out',
-        User: null
+        Description: "User logged out",
+        User: null,
       };
       SendToLog(logData);
 
@@ -413,37 +455,37 @@ const authController = {
     },
   ],
 
-  recovery : async (req, res) => {
+  recovery: async (req, res) => {
     const { email, newPassword } = req.body;
-  
+
     try {
       // Find the user by email
       const user = await User.findOne({ email });
       if (!user) {
         return res.status(404).json({ msg: "User not found" });
       }
-  
+
       // Find the corresponding auth record by user ID
       const auth = await Auth.findOne({ _id: user._id });
       if (!auth) {
         return res.status(404).json({ msg: "Auth record not found" });
       }
-  
+
       // Hash the new password before saving it
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(newPassword, salt);
-  
+
       // Update the password in the auth record
       auth.password = hashedPassword;
       await auth.save();
-  
+
       // Return a success response
       return res.status(200).json({ msg: "Password updated successfully" });
     } catch (err) {
       console.error(err);
       return res.status(500).json({ msg: "Internal server error" });
     }
-  }
+  },
 };
 
 module.exports = authController;
